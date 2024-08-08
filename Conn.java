5e5bb3b8e449e32e728ea787c3db238f006d9f8e
Sakshi Valte <sakshisv270702@gmail.com>
2024-08-08 18:58:14 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+package employee.management.system;
+import java.sql.*;
+
+
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+            
+    
+    
+    
+    public Conn(){
+        try{
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem","root","sakshi270702");
+            s=c.createStatement();
+            
+            
+            
+    }catch(Exception e){
+        e.printStackTrace();
+    
+}
+    }
+}
+
